95974a768e8d757b2aa4d99a223f30c3f12d1672
n-reeves <49203040+n-reeves@users.noreply.github.com>
2025-01-16 00:11:10 +0000

    Deploying to gh-pages from @ n-reeves/n-reeves.github.io@5796750c6d87715e569879a826189dc662678ac7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Quick access to pinned projects. More details on these repos can be found by clicking on the project names.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
